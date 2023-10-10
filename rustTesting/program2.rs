dbcd8b77cee20601e5854daeda06db97276cfa31
TKDuff <thomaskilduff02@gmail.com>
2023-10-10 12:08:26 +0100

    Thread reading from stdin

--- rustTesting/program2.rs
+++ rustTesting/program2.rs
@@ -1,8 +1,9 @@
 use std::io::{self, BufRead};
+use std::thread;
 
 fn main() {
-    let stdin = io::stdin();          // Get the global stdin instance
-    let locked_stdin = stdin.lock(); // Lock stdin for exclusive access
+    // stdin = io::stdin();          // Get the global stdin instance
+    //let locked_stdin = stdin.lock(); // Lock stdin for exclusive access
 
 
     /*
@@ -11,8 +12,19 @@ fn main() {
     If line read succesful iterator return Ok(string) else Err(error)
     Each line in the loop iteration is of type "Result"
     Access string in Result via the unwrap() method
-    */
+    
     for line in locked_stdin.lines() {
         println!("{}", line.unwrap()); 
-    }
+    }*/
+
+    let consumer = thread::spawn(|| {
+        let stdin = io::stdin();          // Get the global stdin instance
+        let locked_stdin = stdin.lock(); // Lock stdin for exclusive access
+        
+        for line in locked_stdin.lines() {
+            println!("Thread print {}", line.unwrap()); 
+        }
+    });
+
+    consumer.join().unwrap();
 }
