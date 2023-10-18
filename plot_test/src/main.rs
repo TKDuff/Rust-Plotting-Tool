c7bd506ef9737b6f56321cc57652bb41e9eb8750
TKDuff <thomaskilduff02@gmail.com>
2023-10-18 10:58:34 +0100

    Threading with egui plot

--- plot_test/src/main.rs
+++ plot_test/src/main.rs
@@ -1,14 +1,14 @@
-//#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
-
 use eframe::egui;
 use egui_plot :: {Line, Plot};
 use std::io::{self, BufRead};
+use std::sync::mpsc;
 use std::thread;
 
 /*create struct for the app, as of now contains points to plot
 */
 struct MyApp {
     points: Vec<[f64; 2]>,   //points to display
+    receiver: mpsc::Receiver<String>, //MyApp only needs the receiving end, not the sending part
 }
 
 impl MyApp {
@@ -24,6 +24,23 @@ impl MyApp {
 default() assigns default values for a type automatically without needing to explicitaly say the type */
 impl Default for MyApp {
     fn default() -> Self {  //returns instance of MyApp
+        let (sender, receiver) = mpsc::channel();
+
+        thread :: spawn(move || {
+            let stdin = io::stdin();          //global stdin instance
+            let locked_stdin = stdin.lock();  //lock stdin for exclusive access
+    
+            for line in locked_stdin.lines() {
+                println!("Sending");
+                if let Ok(line) = line{
+                    sender.send(line).unwrap();
+                }
+                //let line_string = line.unwrap();
+                //println!("sending");
+            }
+        });
+
+
         /*Self {} is the same as MyApp {} , the line below is just initialsing the struct values, point in this case*/
         Self {
             points: vec![
@@ -31,20 +48,13 @@ impl Default for MyApp {
                 [1.0, 1.0],
                 [2.0, 0.5],
                 [3.0, 1.5],],
+            receiver,
         }
     }
 }
 
 fn main() -> Result<(), eframe::Error> {
-
-    thread :: spawn(move || {
-        let stdin = io::stdin();          //global stdin instance
-        let locked_stdin = stdin.lock();  //lock stdin for exclusive access
-
-        for line in locked_stdin.lines() {
-            println!("Thread print {}", line.unwrap()); 
-        }
-    });
+    //let (tx, rx) = mpsc::channel();
 
     /*Window configurations */
     let options = eframe::NativeOptions {
@@ -66,6 +76,11 @@ fn main() -> Result<(), eframe::Error> {
 
 impl eframe::App for MyApp {    //implementing the App trait for the MyApp type, MyApp provides concrete implementations for the methods defined in the App
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) { //'update()' is the method being implemented 
+    while let Ok(point) = self.receiver.try_recv() {
+        //self.points.push(point);
+        println!("New point")
+    }
+
         egui::CentralPanel::default().show(ctx, |ui| { 
             if ui.button("Double").clicked() {
                 self.double_points();
