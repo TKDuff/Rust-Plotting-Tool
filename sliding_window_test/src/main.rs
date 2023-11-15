741dad2bdf094544dcbb7a607022316dde6dacfc
TKDuff <thomaskilduff02@gmail.com>
2023-11-15 14:13:17 +0000

    Working line and box plot

--- sliding_window_test/src/main.rs
+++ sliding_window_test/src/main.rs
@@ -3,23 +3,19 @@ mod raw_data;
 extern crate statrs;
 
 use eframe::{egui, NativeOptions};
-use egui_plot :: {Line, Plot};
-use raw_data::RawData;
+use egui_plot :: {BoxElem, BoxPlot, BoxSpread, Legend, Line, Plot};
+use raw_data::{RawData, Statistics};
 use std::io::{self, BufRead};
 use std::thread;
 use std::sync::{Arc, RwLock}; // Change Mutex to RwLock here
 use crossbeam::channel::unbounded;
-use statrs::statistics::{OrderStatistics, Min, Max};
+use statrs::statistics::{OrderStatistics, Min, Max, Distribution};
 use statrs::statistics::Data;
-
-
-
-
-
 
 /*create struct for the app, as of now contains points to plot*/
 struct MyApp { 
     raw_data: Arc<RwLock<RawData>>, //measurments module
+    plotType: String,
 }
 
 
@@ -29,7 +25,8 @@ impl Default for MyApp {
     fn default() -> Self {  //returns instance of MyApp
         /*Self {} is the same as MyApp {} , the line below is just initialsing the struct values, point in this case*/
         Self {
-            raw_data: Arc::new(RwLock::new(RawData::new(200.0))),
+            raw_data: Arc::new(RwLock::new(RawData::new())),
+            plotType: "Box Plot".to_string(),
 
         }
     }
@@ -45,6 +42,7 @@ fn main() -> Result<(), eframe::Error> {
     let raw_data_thread = app.raw_data.clone();
     let raw_data_for_downsampler = app.raw_data.clone();
     let raw_data_for_egui = app.raw_data.clone();
+    let boxType_for_egui =app.plotType.clone();
 
     let writer = thread::spawn(move ||{
         let stdin = io::stdin();          //global stdin instance
@@ -64,16 +62,23 @@ fn main() -> Result<(), eframe::Error> {
         }
     });
 
+    
     let downsample = thread::spawn(move ||{
-        let mut chunk: Vec<[f64; 2]> = Vec::new();
+        let mut chunk: Vec<Statistics> = Vec::new();
 
         loop {
             if let Ok(points) = receiver.recv() {
-                chunk = raw_data_for_downsampler.read().unwrap().get_chunk(points);
-                let x_for_quartile: Vec<f64> = chunk.iter().map(|&[val, _]| val).collect();
-                let y_for_quartile: Vec<f64> = chunk.iter().map(|&[_, val]| val).collect();
 
+                let (chunk, length) = raw_data_for_downsampler.read().unwrap().get_chunk(points); 
+                
+                //let x_for_quartile: Vec<f64> = chunk.iter().map(|&[val, _]| val).collect();
+                let x_for_quartile: Vec<f64> = chunk.iter().map(|stat| stat.x_avg).collect();
+
+                let y_for_quartile: Vec<f64> = chunk.iter().map(|stat| stat.y_avg).collect();
+
+                
                 println!("\n{:?}\n\n{:?}", x_for_quartile, y_for_quartile);
+                
                 let mut x = Data::new(x_for_quartile);
                 let mut y = Data::new(y_for_quartile);
 
@@ -81,7 +86,25 @@ fn main() -> Result<(), eframe::Error> {
 
                 println!("X:\nLower Quartile: {}\nUpper Quartile: {}\nMedian: {}\nMin: {}\nMax: {}",x.lower_quartile(), x.upper_quartile(), x.median(), x.min(), x.max());
                 println!("\nY:\nLower Quartile: {}\nUpper Quartile: {}\nMedian: {}\nMin: {}\nMax: {}",y.lower_quartile(), y.upper_quartile(), y.median(), x.min(), x.max());
+
+                let singleStat = Statistics {
+                    x_avg: x.median(),
+                    x_lower_quartile: x.lower_quartile(),
+                    x_upper_quartile: x.upper_quartile(),
+                    x_median: x.median(),
+                    x_min: x.min(),
+                    x_max: x.max(),
+
+                    y_avg: y.median(),
+                    y_lower_quartile: y.lower_quartile(),
+                    y_upper_quartile: y.upper_quartile(),
+                    y_median: y.median(),
+                    y_min: y.min(),
+                    y_max: y.max(),
+                };
                 //let quartiles = Quartiles::new
+                raw_data_for_downsampler.write().unwrap().append_chunk(singleStat.clone(), length);
+                raw_data_for_downsampler.write().unwrap().append_box_plot(singleStat);
             }   
         }
 
@@ -106,33 +129,69 @@ fn main() -> Result<(), eframe::Error> {
         }*/
     });
 
-    /*
+    
     let nativ_options = NativeOptions{
         initial_window_size: Some(egui::vec2(960.0, 720.0)),
         ..Default::default()
-    };*/
+    };
 
-    /*
+    
     eframe::run_native("App", nativ_options, Box::new(move |_|{Box::new(MyApp {
-        raw_data: raw_data_for_egui,})}),);
-    */
+        raw_data: raw_data_for_egui, plotType: boxType_for_egui})}),);
+    
     writer.join().unwrap();
     downsample.join().unwrap();
     Ok(())
     
 }
 
-/*
+
 impl eframe::App for MyApp {    //implementing the App trait for the MyApp type, MyApp provides concrete implementations for the methods defined in the App
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) { //'update()' is the method being implemented 
         egui::CentralPanel::default().show(ctx, |ui| { 
+            //let mut plot_type = "Box Plot";
+            ui.radio_value(&mut self.plotType, "Box Plot".to_string(), "Box Plot");
+            ui.radio_value(&mut self.plotType, "Line Plot".to_string(), "Line Plot");
+
+            let box_plot_stats = self.raw_data.read().unwrap().get_box_plot();
+
+            let x_box_plot = BoxPlot::new(vec![
+                BoxElem::new(0.25, BoxSpread::new(box_plot_stats.x_min, box_plot_stats.x_lower_quartile, box_plot_stats.x_median, box_plot_stats.x_upper_quartile, box_plot_stats.x_max))
+                .name("X_Box")
+                .box_width(0.25),
+            ]);
+
+            let y_box_plot = BoxPlot::new(vec![
+                BoxElem::new(0.25, BoxSpread::new(box_plot_stats.y_min, box_plot_stats.y_lower_quartile, box_plot_stats.y_median, box_plot_stats.y_upper_quartile, box_plot_stats.y_max))
+                .name("X_Box")
+                .box_width(0.25),
+            ]);
+
+            let plot_line = Line::new(self.raw_data.read().unwrap().get_plot_values());
+
+            match self.plotType.as_str() {
+                "Box Plot" => {
+                    Plot::new("measurements")
+                        .legend(Legend::default())
+                        .show(ui, |plot_ui| {
+                            plot_ui.box_plot(x_box_plot);
+                            plot_ui.box_plot(y_box_plot);
+                        });
+                    }
+                "Line Plot" => {
+                    Plot::new("measurements")
+                    .show(ui, |plot_ui| {
+                        plot_ui.line(plot_line);
+                    });
+
+                }
+                    _ => {}
+            }
 
 
-            let plot = Plot::new("measurements").allow_zoom(true);
-            plot.show(ui, |plot_ui| {
-                plot_ui.line(Line::new(self.raw_data.read().unwrap().get_values()));//reading from the rawData vector, use read() method with get_values()
-            });
+            
+            
         });
         ctx.request_repaint();  //repaint GUI without needing event
     }
-}*/
+}
--- sliding_window_test/src/raw_data.rs
+++ sliding_window_test/src/raw_data.rs
@@ -1,8 +1,27 @@
+
+#[derive(Default, Debug, Clone)]
+pub struct Statistics {
+    pub x_avg: f64,
+    pub x_lower_quartile: f64,
+    pub x_upper_quartile: f64,
+    pub x_median: f64,
+    pub x_min: f64,
+    pub x_max: f64,
+
+    pub y_avg: f64,
+    pub y_lower_quartile: f64,
+    pub y_upper_quartile: f64,
+    pub y_median: f64,
+    pub y_min: f64,
+    pub y_max: f64,
+}
+
+
 pub struct RawData {
     //each entry in value is array of two float, in the form [x,y]
     //f32, 2 to represent two values, x,y 
-    pub values: Vec<[f64; 2]>,
-    pub window_size: f64,
+    pub values: Vec<Statistics>,
+    pub BoxPlotValue: Statistics,
 }
 
 /*Implementation for measurements 
@@ -12,24 +31,30 @@ Inside function {} 'values' field initialized to the default value of a VecDeque
 
 impl RawData {
     /* Associated function, 'new', creates intance of measurement type with 'values' field initialised with default type */
-    pub fn new(window_size: f64) -> Self{
-        Self { values: Vec::default(),
-        window_size,
+    pub fn new() -> Self{
+        Self { 
+            values: Vec::new(),
+            BoxPlotValue: Statistics {..Default::default()}
      }
     }
     
-    pub fn get_values(&self) -> Vec<[f64; 2]> {
-        self.values.clone().into_iter().collect()
+    pub fn get_plot_values(&self) -> Vec<[f64; 2]> {
+        self.values.iter().map(|stat| [stat.x_avg, stat.y_avg]).collect()
     }
-
-    pub fn get_chunk(&self, start:usize) -> Vec<[f64; 2]> {
+   
+    pub fn get_chunk(&self, start:usize) -> (Vec<Statistics>, usize) {
         let len = self.values.len();
-        self.values[len - start..len].to_vec()  //define end of vector, since new values being pushed in parralel must ensure don't include new values
+        (self.values[len - start..len].to_vec(), len)  //define end of vector, since new values being pushed in parralel must ensure don't include new values
     }
 
+    pub fn append_chunk(&mut self, single_stat: Statistics, end_range: usize) {
+        self.values.splice((end_range - 50)..end_range, std::iter::once(single_stat));
+    }
+
+    /*
     pub fn amend(&mut self, downsampled_section: &Vec<[f64; 2]>, start_range: usize, end_range: usize){
         self.values.splice(start_range..end_range, downsampled_section.iter().cloned());
-    }
+    }*/
 
     /*Takes in line string from standard input, converts two string numbers to float, appends them to the vector of points to plot*/
     pub fn append_str(&mut self, s:&str) {
@@ -39,7 +64,32 @@ impl RawData {
 
     pub fn append_value(&mut self, point: [f64; 2]) {
         println!("{} {} {}", self.values.len(), point[0], point[1]);
-        self.values.push(point);
+        let new_stat = Statistics {
+            x_avg: point[0],
+            y_avg: point[1],
+            ..Default::default()  // Other fields are set to their default values
+        };
+        self.values.push(new_stat);
+    }
+
+    pub fn append_box_plot(&mut self, stats: Statistics) {
+        self.BoxPlotValue = stats;
+        let boxPlot = &self.BoxPlotValue;
+        println!("{} {} {} {}",  boxPlot.x_lower_quartile, boxPlot.x_upper_quartile, boxPlot.x_min, boxPlot.x_max );
+    }
+
+    pub fn get_box_plot (&self) -> Statistics {
+        self.BoxPlotValue.clone()
     }
 
 }
+
+//create a struct with all the statistal values, store this in the 'values' vector
+//writer just append the x,y values of the struct
+//egui just plots the x,y values of the struct
+//downsample takes chunk (so 50 structs, with empty values besides x,y)
+//Downsamples x,y values of 50 structs
+//replace with single struct, stats for x, stats for y
+//this means
+//           box plot is average of chunk
+//           line plot is average of chunk, still showing live plots
