cc978486257708623ecdd3ba9cba58946fa762b7
TKDuff <thomaskilduff02@gmail.com>
2024-02-20 17:39:07 +0000

    Working variable tiers, tested up to 4

--- bin_merging/tierCountMerging/src/main.rs
+++ bin_merging/tierCountMerging/src/main.rs
@@ -155,7 +155,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         let mut merged_CA_last_y_element;
 
         loop {
-            for tier in 0..2 {  //only testing on first tier, initial tier, for now
+            for tier in 0..(num_tiers-1) {  //only testing on first tier, initial tier, for now
                 // let current_tier = tier_vector[tier];
                 // let lower_tier = tier_vector[tier+1];
 
@@ -180,30 +180,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
             }
 
-            /*
-            t1_length = t1_access.read().unwrap().x_stats.len();
-            if t1_length == 4 {
-                process_tier(&t1_access, &t2_access, 7)
-            }
-
-            t2_length = t2_access.read().unwrap().x_stats.len();
-
-            if t2_length == 4 {
-                process_tier(&t2_access, &t3_access, 7)
-            }
-
-            t3_length = t3_access.read().unwrap().x_stats.len();
-
-            if t3_length == 6 {
-                merged_t3_last_x_element = t3_access.write().unwrap().merge_final_tier_vector_bins(3, true);
-                merged_t3_last_y_element = t3_access.write().unwrap().merge_final_tier_vector_bins(3, false);
-                println!("Got the point {:?}", merged_t3_last_x_element);
-                println!("The first elem of t3 was {:?}", t2_access.read().unwrap().x_stats[0]);
-                t2_access.write().unwrap().x_stats[0] = merged_t3_last_x_element;
-                t2_access.write().unwrap().y_stats[0] = merged_t3_last_y_element;
-                println!("Now the first elem of t3 is {:?}", t2_access.read().unwrap().x_stats[0]);
-
-            }*/
         }
     });
 
@@ -243,98 +219,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         "My egui App",native_options,Box::new(move |_|{Box::new(my_app)}),
     );
 
-
-
-    /*
-    let (rd_sender, hd_receiver) = channel::unbounded();
-    let (timer_sender, mut raw_data_receiver) = mpsc::unbounded_channel::<&str>();
-    let (hd_sender, mut rd_receiver) = tokio::sync::mpsc::unbounded_channel::<(f64, f64, usize)>();
-
-
-    let rt = Runtime::new().unwrap();
-
-    let raw_data_thread = my_app.raw_data.clone();
-    let require_external_trigger = raw_data_thread.read().unwrap().requires_external_trigger();
-
-    rt.spawn(async move {
-        let stdin = io::stdin();
-        let reader = BufReader::new(stdin);
-        let mut lines = reader.lines();
-
-        loop {
-            tokio::select! {
-                line = lines.next_line() => {
-                    if let Ok(Some(line)) = line {
-                        raw_data_thread.write().unwrap().append_str(line);
-                        if !require_external_trigger {
-                            if let Some(cut_index) = raw_data_thread.write().unwrap().check_cut() {
-                                rd_sender.send(cut_index).unwrap();
-                            }
-                        }
-                    } else {
-                        // End of input or an error. You can break or handle it as needed.
-                        break;
-                    }
-                }, 
-                //Look into removing this if adwin or count strategies are selected, leaving in for now to make progress
-                aggregate_signal = raw_data_receiver.recv(), if require_external_trigger => {
-                    if let Some(signal) = aggregate_signal{
-                        //re.sender must send data of same type (see cut_index above), if needs be can create ENUM to enscapsulate different type of message, for now its okay since interval message type not important
-                        //rd_sender.send(0).unwrap();
-                    }
-                },
-                /*May not need this anymore...
-                - With tiered approach initial aggregation tier keeps reference to first element of raw data vector
-                point_means_result = rd_receiver.recv() => {
-                    if let Some((x_mean, y_mean, len)) = point_means_result {
-                        raw_data_thread.write().unwrap().remove_chunk(len, (x_mean, y_mean));
-                    }
-                },*/
-            }
-        }
-    });
-
-    /*Asynchronous timer
-    - Nessecary as the tokio loop reads from standard input, which is a blocking task
-    - Upon checking, may not be necessary, thus don't need 'aggregate_signal'*/
-    rt.spawn(async move {
-        let mut interval = time::interval(Duration::from_secs(1));
-
-        loop {
-            interval.tick().await;
-            timer_sender.send("should put something here");
-        }
-    });
-
-
-    let aggregate_thread_raw_data_accessor = my_app.raw_data.clone();
-    let aggregate_thread_aggregate_data_accessor = my_app.aggregate_data_tier.clone();
-
-    
-    //Aggregate points thread
-    thread::spawn(move || {
-        let mut chunk: Vec<[f64;2]>;
-        let mut objective_length = 0;
-        
-        for chunk in hd_receiver {
-            //chunk = aggregate_thread_raw_data_accessor.read().unwrap().get_chunk(message);
-            println!("Processing the chunk: {:?}\n", chunk);
-            aggregate_thread_aggregate_data_accessor.write().unwrap().append_chunk_aggregate_statistics(chunk);
-            //hd_sender.send(aggregate_thread_aggregate_data_accessor.write().unwrap().append_chunk_aggregate_statistics(chunk));
-        }
-
-    });
-
-    /*
-    let native_options = NativeOptions{
-        ..Default::default()
-    };
-
-    eframe::run_native(
-        "My egui App",native_options,Box::new(move |_|{Box::new(my_app)}),
-    );*/
-
-    Ok(())*/
     Ok(())
 }
 
@@ -346,7 +230,7 @@ impl App for MyApp<>  {    //implementing the App trait for the MyApp type, MyAp
 
 
             let mut tier_plot_lines = Vec::new();
-            let colors = [egui::Color32::BLUE, egui::Color32::GREEN, egui::Color32::BLACK, egui::Color32::BROWN];
+            let colors = [egui::Color32::BLUE, egui::Color32::GREEN, egui::Color32::BLACK, egui::Color32::BROWN, egui::Color32::YELLOW];
 
 
             let raw_plot_line = Line::new(self.raw_data.read().unwrap().get_raw_data()).width(2.0).color(egui::Color32::RED);
