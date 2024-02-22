2de455a1cc407c7ab4dae6a327d040491597bd8b
TKDuff <thomaskilduff02@gmail.com>
2024-02-22 19:20:51 +0000

    Basic working count tier merging, however have to fix command lines

--- writer/src/main.rs
+++ writer/src/main.rs
@@ -44,7 +44,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     for result in rdr.deserialize() {
         let record: Record = result?;
         println!("{} {}", record.x_col, record.y_col);
-        thread::sleep(Duration::from_millis(15));
+        thread::sleep(Duration::from_millis(150));
     }
 
     Ok(())
