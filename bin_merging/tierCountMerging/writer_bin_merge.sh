fa4823c49a4a06e0c53dc759e35d3706c8a6b077
TKDuff <thomaskilduff02@gmail.com>
2024-02-24 14:38:43 +0000

    Working interval tiers, however edge case of 1 argument not implemented, have to fix command line arguments to fix it

--- bin_merging/tierCountMerging/writer_bin_merge.sh
+++ bin_merging/tierCountMerging/writer_bin_merge.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #Change the argument after count to set the tiers
 
-(cd ../../writer/ && cargo run --bin writer) | (cargo run --bin tierCountMerging -- "interval" "10" )
+(cd ../../writer/ && cargo run --bin writer) | (cargo run --bin tierCountMerging -- "interval" "2" "6" "8" "20" )
 
 
 #"6" "4" "5" "4" "4"
